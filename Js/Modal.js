c6362a2878d56c574bdf1ea1c37a639653c9089e
hectorbuono <hectorbuono@gmail.com>
2023-05-28 04:14:52 -0300

    probar js ventana modal

--- Js/Modal.js
+++ Js/Modal.js
@@ -1,3 +1,6 @@
 function AbrirModal() {
     document.getElementById("modal").style.display = "block";
   }
+  function CerrarModal() {
+    document.getElementById("modal").style.display = "none";
+  }  
