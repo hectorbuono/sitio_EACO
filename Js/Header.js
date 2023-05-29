3d9611b07310ef6735f5466c663941f2e1a8660f
hectorbuono <hectorbuono@gmail.com>
2023-05-29 01:16:56 -0300

    Subir Header

--- Js/Header.js
+++ Js/Header.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+
 // Crear el elemento de encabezado
 var header = document.createElement('header');
 
@@ -9,76 +11,20 @@ logo.alt = 'Logo de mi sitio web';
 // Crear el elemento de menú
 var menu = document.createElement('nav');
 // Agrega aquí los elementos del menú, como enlaces <a> o elementos <ul> y <li>
-<ul>
-<li><a href="#seccion-2">EQUIPO</a>
-
-<ul>
-
-   <li><a href="#La_Lampalagua">LOGO</a></li>
-
-   <li><a href="#seccion-4">INTEGRANTES</a></li>
-
-  </ul>
-
-</li>
-
-
-<li><a href="#">INVESTIGACIÓN</a>
-
-  <ul>
-
-       <li><a href="Ambato.html">AMBATO</a></li>
-
-   <li><a href="El_Alto.html">EL ALTO</a></li>
-
-  </ul>
-
-</li>
-
-
-<li><a href="#">PROYECTOS</a>
-
-   <ul>
-
-
-     <li><a href="#">UBA</a></li>
-
-     <li><a href="#">CONICET</a></li>
-
-     <li><a href="#">MINCYT</a></li>
-
-     </ul>
-
-  </li>
-
-
-<li><a href="#">PUBLICACIONES</a>
-
-   <ul>
-
-       <li><a href="#">TESIS</a></li>
-
-       <li><a href="#">LIBROS</a></li>
-
-       <li><a href="#">CAPÍTULOS DE LIBRO</a></li>
-
-       <li><a href="#">ARTÍCULOS</a></li>
-
-       <li><a href="#">CONGRESOS</a></li>
-
-   </ul>
-
-</li>   
-
-<li><a href="#">CONTACTO</a></li>
-
-<li><a href="#">ENTRAR</a></li>
-
-</ul>
+var menu = document.createElement('ul');
+var menuItem1 = document.createElement('li');
+menuItem1.textContent = 'Inicio';
+var menuItem2 = document.createElement('li');
+menuItem2.textContent = 'Acerca de';
+var menuItem3 = document.createElement('li');
+menuItem3.textContent = 'Contacto';
 
 // Agregar el logotipo y el menú al encabezado
 header.appendChild(logo);
 header.appendChild(menu);
-
+menu.appendChild(menuItem1);
+menu.appendChild(menuItem2);
+menu.appendChild(menuItem3);
 // Agregar el encabezado a la página
 document.body.insertBefore(header, document.body.firstChild);
+});
